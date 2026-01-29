b3fc4022ce8ce56366a49c26654784cfbab11f6c
Alex Domingo <alex.domingo.toro@vub.be>
2026-01-29 21:53:58 +0100

    move callback functions for help panel into eessi.cli.help module

--- src/eessi/cli/main.py
+++ src/eessi/cli/main.py
@@ -2,36 +2,12 @@
 #
 # authors: Kenneth Hoste (Ghent University)
 
-import click
 import typer
-from rich import print as rich_print
-from typer import rich_utils
 
-import eessi
 from eessi.cli.check import app as check_app
+from eessi.cli.help import help_callback, version_callback
 from eessi.cli.init import app as init_app
 from eessi.cli.shell import app as shell_app
-
-
-def version_callback(value: bool):
-    """
-    Show version and exit early.
-    """
-    if value:
-        rich_print(f"[bold]eessi[/bold] version {eessi.__version__}")
-        raise typer.Exit()
-
-def help_callback(ctx: click.Context, param: click.Parameter, value: bool):
-    """
-    Show default help with rich and exit early.
-    """
-    # ensures this doesn't run during completion or other early parsing phases
-    if not value or ctx.resilient_parsing:
-        return
-
-    # print default help with rich
-    rich_utils.rich_format_help(obj=ctx.command, ctx=ctx, markup_mode="rich")
-    ctx.exit()
 
 app = typer.Typer(
     help="User-friendly command line interface to EESSI - https://eessi.io",
